ee377e87ddff780b583afb8fc6b8a3c25ea7adc4
Javier Alcoriza <jalcoriza@gmail.com>
2023-02-22 19:21:22 +0100

    added SumaLenta.java

---
+++ src/01-introduction/SumaLenta.java
@@ -0,0 +1,13 @@
+public class SumaLenta {
+	public static void main(String [] args) {
+		int a,b;
+		Scanner entrada = new Scanner (System.in);
+		a = entrada.nextInt();
+		b = entrada.nextInt();
+		while (a != 0) {
+			a = a-1;
+			b = b +1;
+		}
+		System.out.println(b);
+	}
+}
