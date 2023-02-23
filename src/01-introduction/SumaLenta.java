360efec931cfe049249ffb6b5a29766a1f630faa
jalcoriza <jalcoriza@gmail.com>
2023-02-23 10:05:28 +0200

    updated files

--- src/01-introduction/SumaLenta.java
+++ src/01-introduction/SumaLenta.java
@@ -4,12 +4,14 @@ public class SumaLenta {
 	public static void main(String [] args) {
 		int a,b;
 		Scanner entrada = new Scanner (System.in);
+		System.out.println("Enter two numbers separated by a space");
 		a = entrada.nextInt();
 		b = entrada.nextInt();
 		while (a != 0) {
 			a = a-1;
 			b = b +1;
 		}
-		System.out.println(b);
+		System.out.printf("The result is %d \n", b);
+		//System.out.println(b);
 	}
 }
